dfb98a31696d9671336d77dca818632bde3622ef
MartaKapocs <marta.kapocs@gmail.com>
2021-06-10 03:11:30 +0200

    To-do-list completed

--- MyFirstApp_CodingCamp_Project5/5.8_JS_To-do-list/scripts.js
+++ MyFirstApp_CodingCamp_Project5/5.8_JS_To-do-list/scripts.js
@@ -1,6 +1,26 @@
-$('label').click(() => {
-    let newItem = $('input').text();
-    $('ul').append('<li></li>');
-    $('li:last').text(newItem);
-    $('input').text('');
+let addBtn = document.querySelector("#addBtn");
+let list = document.querySelector("#list");
+
+let num = 0;
+addBtn.addEventListener("click", function(){
+    let newItem = document.querySelector("#new-item");
+    let li = document.createElement("li");
+    li.innerHTML = "<span id='new_element"+num+"'>" + newItem.value + "</span><span><img id='check_mark"+num+"' src='tick.svg' alt='check-mark'><img id='delete_icon"+num+"' src='delete_icon.svg' alt='Delete Icon'></span>";
+    list.appendChild(li);
+    newItem.value = "";
+    newItem.focus();
+    let check_mark = document.querySelector("#check_mark"+num);
+    let delete_icon = document.querySelector("#delete_icon"+num);
+    let newElement = document.querySelector("#new_element"+num);
+    num++;
+
+    check_mark.addEventListener("click", function(){
+        newElement.style.opacity = "0.5";
+        newElement.style.textDecoration = "line-through";
+        this.style.background = "green";
+    })
+
+    delete_icon.addEventListener("click", function(){
+        this.parentElement.parentElement.remove();
+    })
 });
